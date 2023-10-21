35dddd75c83cdf15ddb7540ce174c94b81a4d190
Bereket Sintayehu <bereketsintayehu32@gmail.com>
2023-10-21 23:59:46 +0300

    Models implemented

---
+++ src/models/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name is required.'],
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price is required.'],
+  },
+  stock: {
+    type: Number,
+    required: [true, 'Product stock is required.'],
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
